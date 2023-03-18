2d5f5a72fd472a0d9e281a5a74ce48356219d01f
FabianUlises <ulisesfab.car@outlook.com>
2023-03-18 05:40:29 -0700

    Added react router dom functionality

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,16 @@
 
 
-export default function Packages() {
-
+export default function Packages(props) {
+    const displayPackages = props.packages.map((item, i) => <li>{item}</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {displayPackages}
+                </ul>
             </div>
         </div>
     )
